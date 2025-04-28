b3bbe2fc618e199c9d802964e4500404a94acfff
Jairus <117532457+jai-rus@users.noreply.github.com>
2025-04-27 21:42:30 -0700

    Updated dataprep

--- dataPrep.py
+++ dataPrep.py
@@ -1,25 +1,57 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
-# Import the necessary dataset 
-df = pd.read_csv("ObesityDataset.csv")
-print(df['Weight'])
+#Set Pandas option to not hide any columns
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('ObesityDataset.csv', sep=',', header=0) #reading the data by using Pandas library
 
+#Checking to see how many unique values there are
+'''
+for col in df.columns:
+    unique_vals = df[col].unique()
+    print(f"Unique values in '{col}': {unique_vals}\n")
+'''
 
-# Scale the data before applying PCA 
-scaling = StandardScaler()
+#Separate into features and truth value
+rawX = df.drop('NObeyesdad', axis=1)
+rawY = df['NObeyesdad']
 
-# Use fit and transform method 
-scaling.fit(df)
-scaled_data = scaling.transform(df)
+#Encode categorical variables
+categorical = ['Gender', 'family_history_with_overweight', 'FAVC', 'CAEC', 'SMOKE', 'SCC', 'CALC', 'MTRANS']
+numerical = ['Age', 'Height', 'Weight', 'FCVC', 'NCP', 'CH2O', 'FAF', 'TUE']
 
-# Set the n_components to 3 
-principal=PCA(n_components=3)
-principal.fit(scaled_data)
-x=principal.transorm(scaled_data)
+#Label the obesity levels
+le = LabelEncoder()
+Y = le.fit_transform(rawY)
 
-# Check the dimensions of the data after PCA
-print(x.shape)
+#Process categorical features categoricalX
+catX = rawX[categorical].apply(LabelEncoder().fit_transform)
+
+#Process numerical features so that it centers the data and normalizes spread
+scaler = StandardScaler()
+numX = pd.DataFrame(scaler.fit_transform(rawX[numerical]), columns=numerical)
+
+#Recombine the new features
+X = pd.concat([numX, catX], axis=1)
+
+#Split data for test set which will be 20%
+X_train_val, X_test, y_train_val, y_test = train_test_split(
+    X, Y,
+    test_size=0.2,
+    random_state=42,
+    stratify=Y  # maintains class distribution
+)
+
+#Separate training and validation from remaining 60% and 20% respectively
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train_val, y_train_val,
+    test_size=0.25,  # 0.25 x 0.8 = 0.2
+    random_state=42,
+    stratify=y_train_val
+)
+
+#Checking sizes to make sure everything is awesome
+print(f"Train size: {len(X_train)} samples ({len(X_train)/len(X):.1%})")
+print(f"Validation size: {len(X_val)} samples ({len(X_val)/len(X):.1%})")
+print(f"Test size: {len(X_test)} samples ({len(X_test)/len(X):.1%})")
