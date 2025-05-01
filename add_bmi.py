5f44642db6ed569a7976e5a0d2a2f0e1bbdb9f5b
jessp60 <jmpinto@cpp.edu>
2025-04-30 20:04:17 -0700

    update to datasets

--- add_bmi.py
+++ add_bmi.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("CategoricalDataset.csv")
 
+bmiSeries = (df['Weight'] / (df['Height'] ** 2)).round(2)
+df.insert(3, "BMI", bmiSeries)
 
-df['BMI'] = (df['Weight'] / (df['Height'] ** 2)).round(2)
-
+df['BMI'] = pd.cut(x=df['BMI'], bins=[0.11, 0.8325, 1.555, 2.277, 3], labels=[1, 2, 3, 4])
 
 df.to_csv("CategoricalDataset.csv", index=False)
 
